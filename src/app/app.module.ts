5d08ba143ea8fcfb6c9d550c2afabb8a45f6c641
= <bing5300@gamil.com>
2018-04-25 11:03:01 +0800

    login post finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HttpModule} from '@angular/http';
+import { IonicApp, IonicErrorHandler, IonicModule,} from 'ionic-angular';
 
 import { LoginPage } from '../pages/login/login';
 
@@ -33,6 +34,7 @@ import { LoginServiceProvider } from '../providers/login-service/login-service';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
-
+import { Http} from '@angular/http';
+import { HomePage } from '../home/home';
 
 @IonicPage()
 @Component({
@@ -9,24 +10,38 @@ import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angul
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public menu: MenuController) {
+  user = {
+    username:null,
+    password:null
+  }
+  @Injectable()
+  constructor(public navCtrl: NavController, public navParams: NavParams, public menu: MenuController,private http: Http) {
     this.menu = menu;
     this.menu.enable(false,"MyMenu")
   }
 
-  logIn(username: HTMLInputElement, password: HTMLInputElement) {
-    if (username.value.length == 0) {
+  login() {
+    if (this.user.username == null) {
         alert("輸入帳號");
-    } else if (password.value.length == 0) {
+    } else if (this.user.password == null) {
         alert("輸入密碼");
       } else {
-        let userinfo: string = 'ID：' + username.value + 'PW：' + password.value;
+        let userinfo: string = 'ID：' + this.user.username + 'PW：' + this.user.password;
         alert(userinfo);
+        this.http.post('http://localhost:9090/app/login?account=',
+          {
+            account : this.user.username,
+            password: this.user.passwd
+          },
+          {
+            headers: { 'Content-Type': 'application/json' }
+          })
+          this.navCtrl.setRoot(HomePage);
+          this.menu.enable(true,"MyMenu")
       }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
-
 }
